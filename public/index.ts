1b68525c18995e92472c2eddfb2edaeb2cf6e06c
rudra <goutam.prasad.2707@gmail.com>
2025-10-26 22:57:27 +0530

    Add images and index file for project assets

---
+++ public/index.ts
@@ -0,0 +1,30 @@
+import question_image from "./images/question_image.png";
+import subject_image from "./images/subject_image.png";
+
+import image_1 from "./images/am-i-here-1.jpg";
+import image_2 from "./images/am-i-here-2.jpg";
+import image_3 from "./images/am-i-here-3.jpg";
+import image_4 from "./images/am-i-here-4.jpg";
+import image_5 from "./images/am-i-here-5.jpg";
+import image_6 from "./images/am-i-here-6.jpg";
+import image_7 from "./images/am-i-here-7.jpg";
+import image_8 from "./images/am-i-here-8.jpg";
+import image_9 from "./images/am-i-here-9.jpg";
+import image_10 from "./images/am-i-here-10.jpg";
+import image_11 from "./images/am-i-here-11.jpg";
+
+export {
+    question_image,
+    subject_image,
+    image_1,
+    image_2,
+    image_3,
+    image_4,
+    image_5,
+    image_6,
+    image_7,
+    image_8,
+    image_9,
+    image_10,
+    image_11,
+};
